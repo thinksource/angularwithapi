89eaeea91d354ae654bc28527d8490d884594596
thinksource <foretribe@gmail.com>
2018-05-11 06:40:28 +1000

    cc

--- src/app/pipe/int2date.pipe.ts
+++ src/app/pipe/int2date.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class Int2datePipe implements PipeTransform {
 
+  //python timestamp change to javascript timestamp
   transform(value: number, args?: any): Date {
     var re = new Date();
     re.setTime(value*1000);
